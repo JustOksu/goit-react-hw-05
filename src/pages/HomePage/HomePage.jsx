359dfbe6b00a15538751bb048f81df43d67ccc1b
Osadchyi Yurii <osa26091994@icloud.com>
2024-10-28 20:42:42 +0200

    v2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { fetchTrendingMovies } from "../../api";
-import MovieList from "../components/MovieList/MovieList";
+import MovieList from "../../components/MovieList/MovieList";
 import styles from "./HomePage.module.css";
 
 const HomePage = () => {
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
-import { useParams, Link, Outlet } from "react-router-dom";
-import { fetchMovieDetails } from "../../api";
+import { useParams, Link, Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchMovieDetails } from "../../api"; // Використовуйте свій шлях до функції API
+import styles from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const location = useLocation();
+  const backLink = location.state?.from ?? "/movies";
 
   useEffect(() => {
     const getMovieDetails = async () => {
@@ -18,19 +21,27 @@ const MovieDetailsPage = () => {
     getMovieDetails();
   }, [movieId]);
 
-  if (!movie) return <div>Loading...</div>;
+  if (!movie) return <p>Loading...</p>;
 
   return (
-    <div>
-      <h2>{movie.title}</h2>
+    <div className={styles.movieDetails}>
+      <Link to={backLink}>Go Back</Link>
+      <h1>{movie.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        alt={movie.title}
+        className={styles.poster}
+      />
       <p>{movie.overview}</p>
       <p>Rating: {movie.vote_average}</p>
-
-      <nav>
-        <Link to="cast">Cast</Link>
-        <Link to="reviews">Reviews</Link>
-      </nav>
-
+      <div className={styles.additionalInfo}>
+        <Link to="cast" state={{ from: backLink }}>
+          Cast
+        </Link>
+        <Link to="reviews" state={{ from: backLink }}>
+          Reviews
+        </Link>
+      </div>
       <Outlet />
     </div>
   );
